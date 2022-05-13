8aa3a717e575983e579f42a37d2914e1fe14dc81
chrisrYan <chrisryanbaltazar@gmail.com>
2022-05-13 10:34:27 +0800

    first commit

---
+++ app/src/main/java/com/example/navigationpractice/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.navigationpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view){
+        openDrawer(drawerLayout);
+
+    }
+    public static void openDrawer(DrawerLayout drawerLayout){
+
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void ClickLogo(View view){
+        closeDrawer(drawerLayout);
+
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void ClickAdd(View view){
+        redirectActivity(this,Add.class);
+    }
+
+    public void ClickEdit(View view){
+        redirectActivity(this,Edit.class);
+    }
+    public void ClickDelete(View view){
+        redirectActivity(this,Delete.class);
+    }
+
+    public static void redirectActivity(Activity activity,Class aClass){
+        Intent intent = new Intent(activity,aClass);
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+
+}
